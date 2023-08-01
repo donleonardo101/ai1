d12a6292dfbf57b1a31f971db7f67420fd8db300
chrisgiem <krzysztofgiemza1@gmail.com>
2023-08-01 21:07:57 +0200

    demo ok

--- src/components/Demo.jsx
+++ src/components/Demo.jsx
@@ -1,12 +1,12 @@
-import { useState, useEffect } from 'react';
+import React, { useState, useEffect } from "react";
 
 import { copy, linkIcon, loader, tick } from '../assets';
 import { useLazyGetSummaryQuery } from '../services/article';
 
 const Demo = () => {
   const [article, setArticle] = useState({
-    url: '',
-    summary: '',
+    url: "",
+    summary: "",
   });
   const [allArticles, setAllArticles] = useState([]);
   const [copied, setCopied] = useState("");
@@ -15,83 +15,96 @@ const Demo = () => {
 
   useEffect(() => {
     const articlesFromLocalStorage = JSON.parse(
-      localStorage.getItem('articles');
-    )
+      localStorage.getItem("articles")
+    );
 
     if (articlesFromLocalStorage) {
-      setAllArticles(articlesFromLocalStorage)
-      // 56:31
+      setAllArticles(articlesFromLocalStorage);
     }
   }, []);
 
   const handleSubmit = async (e) => {
     e.preventDefault();
 
+    const existingArticle = allArticles.find(
+      (item) => item.url === article.url
+    );
+
+    if (existingArticle) return setArticle
+      (existingArticle);
+
     const { data } = await getSummary({ articleUrl: article.url });
 
-    if(data?.summary) {
+    if (data?.summary) {
       const newArticle = { ...article, summary: data.summary };
       const updatedAllArticles = [newArticle, ...allArticles];
 
       setArticle(newArticle);
       setAllArticles(updatedAllArticles);
-
-      localStorage.setItem('articles', JSON.stringify(updatedAllArticles))
+      localStorage.setItem("articles", JSON.stringify(updatedAllArticles));
     }
-  }
+  };
 
   const handleCopy = (copyUrl) => {
     setCopied(copyUrl);
     navigator.clipboard.writeText(copyUrl);
-  }
+    setTimeout(() => setCopied(false), 3000);
+  };
+
+  const handleKeyDown = (e) => {
+    if (e.keyCode === 13) {
+      handleSubmit(e);
+    }
+  };
 
   return (
-    <section className="mt-16 w-full max-w-xl">
+    <section className='mt-16 w-full max-w-xl'>
       {/* Search */}
-      <div className="flex flex-col w-full gap-2">
+      <div className='flex flex-col w-full gap-2'>
         <form
-          className="relative flex justify-center items-center"
+          className='relative flex justify-center items-center'
           onSubmit={handleSubmit}
         >
-          <img 
+          <img
             src={linkIcon}
-            alt="link_icon"
-            className="absolute left-0 my-2 ml-3 w-5"
+            alt='link-icon'
+            className='absolute left-0 my-2 ml-3 w-5'
           />
 
-          <input 
-            type="url"
-            placeholder="Enter a website address"
+          <input
+            type='url'
+            placeholder='Wprowadź adres strony'
             value={article.url}
             onChange={(e) => setArticle({ ...article, url: e.target.value })}
+            onKeyDown={handleKeyDown}
             required
-            className="url_input peer"
+            className='url_input peer'
           />
-
           <button
-            type="submit"
-            className="submit_btn peer-focus:border-gray-700 peer-focus:text-gray-700"
+            type='submit'
+            className='submit_btn peer-focus:border-gray-700 peer-focus:text-gray-700 '
           >
-            ↲
+            <p>↲</p>
           </button>
         </form>
 
-        {/* Browse URL History */}
-        <div className="flex flex-col gap-1 max-h-60 overflow-y-auto">
-          {allArticles.map((item, index) => (
+        {/* przegladanie hisotrii linkow */}
+        <div className='flex flex-col gap-1 max-h-60 overflow-y-auto'>
+          {allArticles.reverse().map((item, index) => (
             <div
               key={`link-${index}`}
               onClick={() => setArticle(item)}
-              className="link_card"
+              className='link_card'
             >
-              <div className="copy_btn">
-                <img 
-                  src={copy}
-                  alt="copy_icon"
-                  className="w-[40%] h-[40%] object-contain"
+              <div className='copy_btn' onClick={() => handleCopy(item.url)}>
+                <img
+                  src={copied === item.url ? tick : copy}
+                  alt={copied === item.url ?
+                  "tick_icon" : "copy_icon"}
+                  className='w-[40%] h-[40%] object-contain'
                 />
               </div>
-              <p className="flex-1 font-satoshi text-blue-700 font-medium text-sm truncate">
+              <p className='flex-1 font-satoshi text-blue-700 font-medium text-sm truncate'>
                 {item.url}
               </p>
             </div>
@@ -99,8 +112,8 @@ const Demo = () => {
         </div>
       </div>
 
-      {/* Display Results */}
-      <div className="my-10 max-w-full flex justify-center items-center">
+      {/* wyswietlanie wyniku */}
+      <div className='my-10 max-w-full flex justify-center items-center'>
             {isFetching ? (
               <img src={loader} alt="loader" className="w-20 h-20 object-contain" />
             ) : error ? (
@@ -127,7 +140,7 @@ const Demo = () => {
             )}
       </div>
     </section>
-  )
-}
+  );
+};
 
 export default Demo
